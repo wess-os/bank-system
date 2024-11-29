f82e4e9a8f84927795e3bde84ff9af736f414eb2
Wesley <wes.lol25@gmail.com>
2024-11-29 10:59:57 -0300

    feat(main): config error handling in sequelize

--- src/infrastructure/database/sequelize.module.ts
+++ src/infrastructure/database/sequelize.module.ts
@@ -1,24 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { SequelizeModule as NestSequelizeModule } from '@nestjs/sequelize';
 import { Cliente } from '../../domain/entities/cliente.entity';
 import { ContaBancaria } from '../../domain/entities/conta-bancaria.entity';
 import { Movimentacao } from '../../domain/entities/movimentacao.entity';
+import { Sequelize } from 'sequelize-typescript';
 
 @Module({
     imports: [
         NestSequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'bank-system',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
             autoLoadModels: true,
             synchronize: false,
-            logging: console.log,
             models: [Cliente, ContaBancaria, Movimentacao],
         }),
     ],
     exports: [NestSequelizeModule],
 })
-export class SequelizeModule { }
+
+export class SequelizeModule implements OnModuleInit {
+    constructor(private sequelize: Sequelize) { }
+
+    async onModuleInit() {
+        try {
+            await this.sequelize.sync({ force: false });
+
+            console.log('Banco de dados sincronizado!');
+        } catch (error) {
+            console.error('Erro ao sincronizar o banco de dados:', error);
+        }
+    }
+}
