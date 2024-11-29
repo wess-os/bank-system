d2425c0413ffa44672a311d2fde30389396921b8
Wesley <wes.lol25@gmail.com>
2024-11-29 10:57:54 -0300

    feat(main): set swagger url on console log

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,15 +7,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('Sistema Bancário')
     .setDescription('API do sistema bancário')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  console.log('Swagger está disponível em: http://localhost:3001/api');
+
+  await app.listen(3001);
 }
 bootstrap();
